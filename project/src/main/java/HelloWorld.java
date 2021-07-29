f95e8c20288cc7cc136217cc2a4d510603569392
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2021-07-29 16:32:17 +0530

    Update HelloWorld.java

--- project/src/main/java/HelloWorld.java
+++ project/src/main/java/HelloWorld.java
@@ -15,9 +15,17 @@ public class HelloWorld {
     
     public void sayHello() {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
    
     public void notCovered() {
+        System.out.println("This method is not covered by unit tests");
+        System.out.println("This method is not covered by unit tests");
+        System.out.println("This method is not covered by unit tests");
+        System.out.println("This method is not covered by unit tests");
         System.out.println("This method is not covered by unit tests");
     }
 
